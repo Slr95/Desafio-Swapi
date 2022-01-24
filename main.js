1271a968d1765ab7b67226cd3196e033292bb25b
ReSlr <renzo_soler@hotmail.com>
2022-01-23 22:15:20 -0300

    master

--- main.js
+++ main.js
@@ -3,35 +3,33 @@
 //dar una respuesta si algo anda mal en ese fetch por consola con el catch. En esas 3 promesas
 //Cosas secundarias para hacer: mejorar el codigo, agregar el catch, hacerlo con asyc await, agregar css
 
-const loadJson = () => {
-    return fetch('https://swapi.dev/api/films')
-        .then(response => response.json())
-        .then(json => {
-            const list = document.querySelector('ul')
-            for(let i = 0; i < json.results.length; i++) {
-                const movie = document.createElement('li')
-                movie.innerHTML = `<h3>${json.results[i].title}</h3>`
-                movie.innerHTML += `<p> Episode ${json.results[i].episode_id}</p>`
-                movie.innerHTML += `<p>${json.results[i].opening_crawl}</p>`
-                list.appendChild(movie)
-            }
-        }).catch((err) => {
-            console.log('Hubo un error cargando las peliculas!' + err);
-        })
-        .then(json=> {      
-            const characterList = json.results.map((results)=> {
-                let castList = '<ul>'
-                const toons = results.characters.map(async (toon)=>await Promise.all(toon).then(res=>res.json()))
-                results.characters = toons;
-                return results;
-                })
-                castList += '</ul>'
-                movie.innerHTML += `Cast: ${castList}`
-                json.results = characterList;
-               return json; 
-               
-        
-        }).catch((err) => {
-            console.log('Hubo un error cargando los personajes!' + err);
-        })
+const loadJson = async () => {
+    const response = await fetch('https://swapi.dev/api/films')
+    const json = await response.json()
+    const list = document.querySelector('ul')
+    for (let i = 0; i < json.results.length; i++) {
+        const movie = document.createElement('li')
+        movie.innerHTML = `<h3>${json.results[i].title}</h3>`
+        movie.innerHTML += `<p> Episode ${json.results[i].episode_id}</p>`
+        movie.innerHTML += `<p>${json.results[i].opening_crawl}</p>`
+        let castList = '<ul>'
+        let urlNames = json.results[i].characters.map(async url => {
+            return `<li>${await getCharacters(url)}</li>`})
+        let results = await Promise.all(urlNames)
+        castList += results.join("")
+        console.log(results);
+        castList += '</ul>'
+        movie.innerHTML += `Cast: ${castList}`
+        list.appendChild(movie)
+    }
+}
+
+const getCharacters = async (url) => {
+    try {
+        const response = await fetch(url)
+        const data = await response.json()
+    return data.name
+    } catch(err) {
+        console.log(err);
+    }
 }
